58942be33a06487a0fe9689538746fdd1d19cb3d
MJ-Fon <mici.angels.gaia@gmail.com>
2022-09-15 21:57:58 +0200

    osnovni koncepti rutiranja

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div  >
     <BrowserRouter  >
-      <NavBar ></NavBar>
+      <Navbar ></Navbar>
       <Routes>
        
         
