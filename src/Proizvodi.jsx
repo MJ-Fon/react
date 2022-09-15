53c2840f0b86c80d42ba7133ce8277b039945ee9
MJ-Fon <mici.angels.gaia@gmail.com>
2022-09-15 22:31:15 +0200

    omoguceno sortiranje proizvoda po ceni

--- src/Proizvodi.jsx
+++ src/Proizvodi.jsx
@@ -1,19 +1,36 @@
 import ProizvodKartica from "./ProizvodKartica";
 
  
- 
+import { useState } from "react";
 function Proizvodi({proizvodi,onAdd, onRemove}) {
+  const [sort, setSort] = useState(true);
+  function sortAsc(){
+    
+    setSort(true);
+  }
+  function sortDesc(){
+    
+    setSort(false);
+  }
   return (
+    <div>
+      <div className="buttons">
+      <button className="sortbtn btn" onClick={sortAsc}>Sortiraj rastuće</button>
+      <button className="sortbtn btn" onClick={sortDesc}>Sortiraj opadajuće</button>
+    </div>
+      <div className="all-products">
+      {sort === true ? 
+         proizvodi
+             .sort((a, b) => a.price < b.price ? -1 : 1)
+            .map((p) => ( <ProizvodKartica product={p} key={p.id} onAdd={onAdd} onRemove={onRemove}  />  ))
+          :
+          proizvodi
+          .sort((a, b) => a.price > b.price ? -1 : 1)
+         .map((p) => ( <ProizvodKartica product={p} key={p.id} onAdd={onAdd} onRemove={onRemove}  />  ))
+      }
 
-    <div className="all-products">
-        {proizvodi .map((p) => (
-    
-        <ProizvodKartica product={p} key={p.id} onAdd={onAdd} onRemove={onRemove}  />
-
-    ))}
-
+    </div>
    </div>
-    
   );
 }
 
